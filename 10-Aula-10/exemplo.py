a67699c2fb718e636502cca4668eb992e6952c78
˜Felipe˜ <eng.felipeabreu@gmail.com>
2024-08-03 15:14:59 -0300

    Adicionando os arquivos da aula 10

---
+++ 10-Aula-10/exemplo.py
@@ -0,0 +1,37 @@
+from sqlalchemy import create_engine
+
+# Conectar ao SQLite em memória
+engine = create_engine('sqlite:///meubanco.db', echo=True)
+
+## dialetos
+## engine = create_engine("postgresql+psycopg2://scott:tiger@localhost:5432/mydatabase")
+
+
+print("Conexão com SQLite estabelecida.")
+
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__ = 'usuarios'
+
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    idade = Column(Integer)
+
+#Criar as tabelas no banco de dados
+Base.metadata.create_all(engine)
+
+from sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+novo_usuario = Usuario(nome='João', idade=28)
+session.add(novo_usuario)
+session.commit()
+
+print("Usuário inserido com sucesso.")
+
